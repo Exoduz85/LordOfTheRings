677ef9edef004c80fd3783613835d1264f0b5269
Exoduz85 <47660094+Exoduz85@users.noreply.github.com>
2020-10-20 12:20:14 +0200

    Adds save and load
    adds save and load function

--- Assets/Scripts/Gold.cs
+++ Assets/Scripts/Gold.cs
@@ -6,16 +6,22 @@ using TMPro;
 public class Gold : MonoBehaviour
 {
     private TextMeshProUGUI goldAmountText;
-    private int gold = 0;
+    private int gold;
     public int GoldAmount {get => this.gold; set => this.gold += value; }
 
     private void Start()
     {
+        this.gold = PlayerPrefs.GetInt("Gold", 0);
         goldAmountText = GetComponent<TextMeshProUGUI>();
     }
 
     private void Update()
     {
         goldAmountText.text = GoldAmount.ToString();
+    }
+
+    private void OnDestroy()
+    {
+        PlayerPrefs.SetInt("Gold", gold);
     }
 }
