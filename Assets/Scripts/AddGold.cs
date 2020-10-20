67221f18ac03016cbf7f2c7519ac4eb4b522f2d5
Exoduz85 <47660094+Exoduz85@users.noreply.github.com>
2020-10-20 11:39:17 +0200

    gold
    added gold

---
+++ Assets/Scripts/AddGold.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddGold : MonoBehaviour
+{
+    public int addedGold = 5;
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Gold>().GoldAmount = addedGold;
+        }
+    }
+}
---
+++ Assets/Scripts/Gold.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class Gold : MonoBehaviour
+{
+    private TextMeshProUGUI goldAmountText;
+    private int gold;
+    public int GoldAmount {get => this.gold; set => this.gold += value; }
+
+    private void Start()
+    {
+        goldAmountText = GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Update()
+    {
+        goldAmountText.text = GoldAmount.ToString();
+    }
+}
